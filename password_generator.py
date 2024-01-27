05a52f2ef98c32388cbe9cec2487b4f34c0b8d73
Josh Nelson <joshuaknelson1202@icloud.com>
2024-01-27 10:46:58 -0700

    random generation/bugs with length of pwd

--- password_generator.py
+++ password_generator.py
@@ -1,7 +1,13 @@
 import random
 import string
 
-def generate_password(min_length = 7, max_length = 16, numbers = True, special_characters = True):
+def generate_password(reqs):
+    min_length = reqs[0]
+    max_length = reqs[1]
+    numbers = reqs[2]
+    special_characters = reqs[3]
+    length = random.randint(min_length, max_length)
+
     # populate variables with associated ASCII characters
     letters = string.ascii_letters
     digits = string.digits
@@ -20,8 +26,9 @@ def generate_password(min_length = 7, max_length = 16, numbers = True, special_c
     has_number = False
     has_special = False
 
-    while not meets_requirements or len(password) < min_length:
+    while not meets_requirements or len(password) != length:
         new_char = random.choice(characters)
+        password += new_char
         if new_char in digits:
             has_number = True
         elif new_char in specials:
@@ -32,8 +39,50 @@ def generate_password(min_length = 7, max_length = 16, numbers = True, special_c
             meets_requirements = has_number
         if special_characters:
             meets_requirements = meets_requirements and has_special
+
+        if meets_requirements:
+            return password
+
 # min_length, max_length, numbers, specials
+def get_pwd_requirements():
+    while True:
+        try:
+            min = int(input("Enter an integer for MIN length of password : "))
+            break
+        except ValueError:
+            print("Invalid Character Type. Please Try Again")
+    while True:
+        try:
+            max = int(input("Enter an integer for MAX length of password : "))
+            break
+        except ValueError:
+            print("Invalid Character Type. Please Try Again")
+    while True:
+        nums_in_pwd = input("Would you like to have numbers in the password (y/n): ")
+        if (nums_in_pwd == 'y'):
+            nums = True
+            break
+        elif (nums_in_pwd == 'n'):
+            nums = False
+            break
+        else:
+            print("Type 'y' or 'n'. Please Try Again")
+    while True:
+        special_in_pwd = input("Would you like to have special characters in the password (y/n): ")
+        if (special_in_pwd == 'y'):
+            specials = True
+            break
+        elif (special_in_pwd == 'n'):
+            specials = False
+            break
+        else:
+            print("Type 'y' or 'n'. Please Try Again")
+    return [min, max, nums, specials]
 
 
+reqs = get_pwd_requirements()
 
+
+password = generate_password(reqs)
+print(password)
 
