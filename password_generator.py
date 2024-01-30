fb78c9569598fc9e8fb60b30c3724633568444eb
Josh Nelson <joshuaknelson1202@icloud.com>
2024-01-30 07:53:48 -0700

    cleared up structure using in-line comments

--- password_generator.py
+++ password_generator.py
@@ -2,12 +2,15 @@ import random
 import string
 from random import seed
 from random import randint
+
+#generate password
 def generate_password(reqs):
     min_length = reqs[0]
     max_length = reqs[1]
     numbers = reqs[2]
     special_characters = reqs[3]
     length = random.randint(min_length, max_length)
+
     # populate variables with associated ASCII characters
     letters = string.ascii_letters
     digits = string.digits
@@ -24,15 +27,17 @@ def generate_password(reqs):
     meets_requirements = False
     has_number = False
     has_special = False
-    num = randint(min_length, max_length)
-    counter = 0
+
+    #While loop until valid password is created
     while True:
         password = ""
-        for i in range(num):
+        # populate the string called 'password'
+        for i in range(length):
             new_char = random.choice(characters)
             password += new_char
+        #checks criteria for password (n & s/n/s)
         if numbers and special_characters:
-            for i in range(num):
+            for i in range(length):
                 if password[i] in digits:
                     has_number = True
                 if password[i] in specials:
@@ -40,38 +45,42 @@ def generate_password(reqs):
             if has_number and has_special:
                 meets_requirements = True
         elif numbers:
-            for i in range(num):
+            for i in range(length):
                 if password[i] in digits:
                     has_number = True
                     meets_requirements = True
         elif special_characters:
-            for i in range(num):
+            for i in range(length):
                 if password[i] in specials:
                     has_special = True
                     meets_requirements = True
         else:
             meets_requirements = True
+        #end while loop if the password meets the criteria given by use
         if meets_requirements:
             break
+    #return password
     return password
 
 
 
-# min_length, max_length, numbers, specials
-def get_pwd_requirements():
+#gets password reqs in a list by utilizing while loops and conditionals
+def get_pwd_reqs():
+    #min input
     while True:
         try:
             min = int(input("Enter an integer for MIN length of password : "))
             break
         except ValueError:
             print("Invalid Character Type. Please Try Again")
+    #max input
     while True:
         try:
             max = int(input("Enter an integer for MAX length of password : "))
             break
         except ValueError:
             print("Invalid Character Type. Please Try Again")
-
+    #numbers
     while True:
         nums_in_pwd = input("Would you like to have numbers in the password (y/n): ")
         if (nums_in_pwd == 'y'):
@@ -82,6 +91,7 @@ def get_pwd_requirements():
             break
         else:
             print("Type 'y' or 'n'. Please Try Again")
+    #special characters
     while True:
         special_in_pwd = input("Would you like to have special characters in the password (y/n): ")
         if (special_in_pwd == 'y'):
@@ -92,13 +102,17 @@ def get_pwd_requirements():
             break
         else:
             print("Type 'y' or 'n'. Please Try Again")
+    #return list of reqs
     return [min, max, nums, specials]
 
 seed(1)
 
-reqs = get_pwd_requirements()
+#get reqs for password
+reqs = get_pwd_reqs()
 
+#generate password
 password = generate_password(reqs)
-print()
+
+#display password
 print("Password: " + str(password))
 
